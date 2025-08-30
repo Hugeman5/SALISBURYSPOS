e77b488f2d9ee16a67ad94a07128f5c6e4946de7
Eugene van der Westhuizen <eugene@salisburys.co.za>
2025-08-30 16:16:01 +0000

    functions/src/orders.ts - import {format} from "date-fns"; - import {db,

--- functions/src/orders.ts
+++ functions/src/orders.ts
@@ -5,8 +5,7 @@
 import {onCall, HttpsError} from "firebase-functions/v2/https";
 import * as admin from "firebase-admin";
 import {z} from "zod";
-import {format} from "date-fns";
-import {db, requireRole, Role} from "./utils";
+import {db, requireRole} from "./utils";
 
 const VAT_RATE = 0.15;
 
