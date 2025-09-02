88b648c2b4dd93e0c01587afbd2713a389041169
Eugene van der Westhuizen <eugene@salisburys.co.za>
2025-09-02 08:31:47 +0000

    i dont see any of the changes in built we made reflecting on admin dashb

--- src/app/dashboard/admin/layout.tsx
+++ src/app/dashboard/admin/layout.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 import {
   Sidebar,
@@ -22,6 +23,8 @@ import {
   Home,
   ShoppingCart,
   Moon,
+  Sheet,
+  PenSquare,
 } from 'lucide-react';
 import { usePathname } from 'next/navigation';
 import { LogoutButton } from '@/components/auth/logout-button';
@@ -33,8 +36,10 @@ const menuItems = [
   { href: '/dashboard/admin', title: 'Dashboard', icon: Home, roles: ['admin', 'manager'] },
   { href: '/pos/sale', title: 'Point of Sale', icon: ShoppingCart, roles: ['admin', 'manager', 'cashier', 'waiter', 'kitchen'] },
   { href: '/dashboard/admin/orders', title: 'Orders', icon: Receipt, roles: ['admin', 'manager', 'cashier'] },
+  { href: '/dashboard/admin/menu', title: 'Menu Builder', icon: PenSquare, roles: ['admin', 'manager'] },
   { href: '/dashboard/admin/products', title: 'Products', icon: Package, roles: ['admin', 'manager'] },
   { href: '/dashboard/admin/inventory', title: 'Inventory', icon: LayoutGrid, roles: ['admin', 'manager'] },
+  { href: '/dashboard/admin/floor-builder', title: 'Floor Plan', icon: Sheet, roles: ['admin', 'manager'] },
   {
     href: '/dashboard/admin/cash-register',
     title: 'Cash Register',
